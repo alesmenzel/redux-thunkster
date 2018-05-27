54a237b0fe1d6727376394d40cb0d254ed1a8655
Aleš Menzel <alesmenzel@gmail.com>
2018-05-27 14:09:19 +0200

    linter, gitignore, formatting

--- example/index.js
+++ example/index.js
@@ -1,86 +1,70 @@
-const {
-  createStore,
-  applyMiddleware
-} = require('redux');
+const { createStore, applyMiddleware } = require('redux');
 const thunkster = require('../src/thunkster');
 
 const reducer = (state = {}, action) => {
   return {
     ...state,
-    ...action.payload
+    ...action.payload,
   };
-}
+};
 
 store = createStore(reducer, applyMiddleware(thunkster));
-
-
-
-
-
 
 const handler = (action, dispatch, getState) => {
   if (action.type === 'ACTION') {
     dispatch({
       type: 'ACTION_LISTENER',
       payload: {
-        listened: true
-      }
-    })
+        listened: true,
+      },
+    });
   }
-}
+};
 
 const unsubscribe = thunkster.subscribe(handler);
 
-
-
-
-
-
-
 store.dispatch({
   type: 'ACTION',
   payload: {
     a: 5,
-    b: 15
-  }
+    b: 15,
+  },
 });
 
 store.dispatch({
   type: 'ACTION2',
   payload: {
     a: 5,
-  }
+  },
 });
 
 store.dispatch({
   type: 'ACTION3',
   payload: {
-    b: 15
-  }
+    b: 15,
+  },
 });
-
 
 unsubscribe();
 
-
 store.dispatch({
   type: 'ACTION',
   payload: {
     a: 5,
-    b: 15
-  }
+    b: 15,
+  },
 });
 
 store.dispatch({
   type: 'ACTION2',
   payload: {
     a: 5,
-  }
+  },
 });
 
 store.dispatch({
   type: 'ACTION3',
   payload: {
-    b: 15
-  }
+    b: 15,
+  },
 });
--- src/thunkster.js
+++ src/thunkster.js
@@ -1,21 +1,21 @@
 const thunkster = store => next => action => {
   console.log(store.getState(), action);
 
-  thunkster.listeners.forEach((listener) => {
+  thunkster.listeners.forEach(listener => {
     listener(action, store.dispatch, store.getState);
   });
 
   next(action);
-}
+};
 
 thunkster.listeners = [];
 
-thunkster.unsubscribe = (listener) =>
-  thunkster.listeners = thunkster.listeners.filter((item) => item !== listener);
+thunkster.unsubscribe = listener =>
+  (thunkster.listeners = thunkster.listeners.filter(item => item !== listener));
 
-thunkster.subscribe = (listener) => {
+thunkster.subscribe = listener => {
   thunkster.listeners.push(listener);
-  return () => thunkster.unsubscribe(listener)
+  return () => thunkster.unsubscribe(listener);
 };
 
 module.exports = thunkster;
